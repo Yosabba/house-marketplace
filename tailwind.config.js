21033bacd7e8b2655727792848dfb9e359fa0e29
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-06-07 23:10:09 -0400

    :zap: added media breakpoints and grid template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      // => @media (min-width: 480px) { ... }
+
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(350px, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
