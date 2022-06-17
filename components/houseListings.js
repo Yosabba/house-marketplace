c216a1f9811f045e5e22aaf949edabf6bd95cab0
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-06-16 22:36:18 -0400

    :zap: added houses listing comp

--- components/houseListings.js
+++ components/houseListings.js
@@ -1,6 +1,6 @@
 const HouseListing = () => {
   return (
-    <div className="flex flex-col w-5/6 cursor-pointer items-center justify-center rounded-lg bg-white min-h-[30vh] hover:shadow-xl transition duration-500">
+    <div className="flex flex-col w-[50rem] cursor-pointer items-center justify-center rounded-lg bg-white min-h-[30vh] hover:shadow-xl transition duration-500">
       <h1>House Listing</h1>
       <h3>image</h3>
       <p>name</p>
