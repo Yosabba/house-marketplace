fe015ac7e1fffd215bf9837718cad2568a559f3a
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-06-16 22:30:55 -0400

    :sparkles: new component for recommend slider

---
+++ components/RecommendSlider.js
@@ -0,0 +1,7 @@
+const RecommendedSlider = () => {
+  return (
+    <div className="bg-red-600 w-5/6 mx-auto text-center min-h-[20rem] rounded-lg mb-8"></div>
+  );
+};
+
+export default RecommendedSlider;
